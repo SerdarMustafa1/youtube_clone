78d5fe3a99233e5fea8b6abbe5b443c4f81c4fef
Serdar Mustafa <serdarmustafa@gmail.com>
2019-03-31 19:47:04 +0300

    Basic Youtube clone using Vue framework

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+// Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
+
+//el: '#app', alternate option to load the HTML
+//ES5
+// new Vue({
+//   render: function(createElement) {
+//     return createElement(App);
+//   }
